d37518e30a0e80db2f185902252d45db49590da8
Rvail <naveedm@tcd.ie>
2019-05-24 01:05:01 +0100

    error handling

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+app.use(morgan('dev'));
+
+// Routes for handling requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+// If this part of code reached, then no routes could handle request
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+// Handle errors thrown from anywhere else in code
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
