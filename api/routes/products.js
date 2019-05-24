839418213323376dc5334acd0e26b90e41f13cf2
Rvail <naveedm@tcd.ie>
2019-05-25 00:57:10 +0100

    better responsnses, links to other resources

--- api/routes/products.js
+++ api/routes/products.js
@@ -6,10 +6,24 @@ const Product = require('../models/product');
 // Return all products in database
 router.get('/', (req, res, next) => {
     Product.find()
+        .select('name price _id')
         .exec()
         .then(docs => {
-            console.log(docs);
-            res.status(200).json(docs);
+            const response = {
+                count: docs.length,
+                products: docs.map(doc => {
+                    return {
+                        name: doc.name,
+                        price: doc.price,
+                        _id: doc._id,
+                        request: {
+                            type: 'GET',
+                            url: 'http://localhost:3000/products/' + doc._id
+                        }
+                    }
+                })
+            };
+            res.status(200).json(response);
         })
         .catch(err => {
             console.log(err);
@@ -28,10 +42,17 @@ router.post('/', (req, res, next) => {
     });
 
     product.save().then(result => {
-        console.log(result);
         res.status(201).json({
-            message: 'handling POST requests to /products',
-            createdProduct: result
+            message: 'Created product',
+            createdProduct: {
+                name: result.name,
+                price: result.price,
+                _id: result._id,
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:3000/products/' + result._id
+                }
+            }
         });
     })
         .catch(err => {
@@ -45,10 +66,19 @@ router.post('/', (req, res, next) => {
 // Find a specific product
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
-    Product.findById(id).exec().then(doc => {
+    Product.findById(id)
+    .select('name price _id')
+    .exec()
+    .then(doc => {
         console.log('From Database', doc);
         if (doc) {
-            res.status(200).json(doc);
+            res.status(200).json({
+                product: doc,
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:3000/products'
+                }
+            });
 
         } else {
             res.status(404).json({
@@ -56,10 +86,10 @@ router.get('/:productId', (req, res, next) => {
             });
         }
     })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({ error: err });
-        });
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({ error: err });
+    });
 });
 
 // Update properties of existing product
@@ -73,8 +103,13 @@ router.patch('/:productId', (req, res, next) => {
     Product.update({ _id: id }, { $set: updateOps })
         .exec()
         .then(result => {
-            console.log(result);
-            res.status(200).json(result);
+            res.status(200).json({
+                message: 'Product updated',
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:3000/products/' + id
+                }
+            });
         })
         .catch(err => {
             console.log(err);
@@ -87,10 +122,17 @@ router.patch('/:productId', (req, res, next) => {
 // Delete a product in database
 router.delete('/:productId', (req, res, next) => {
     const id = req.params.productId;
-    Product.remove({ _id: id })
+    Product.deleteOne({ _id: id })
         .exec()
         .then(result => {
-            res.status(200).json(result)
+            res.status(200).json({
+                message: 'Product deleted',
+                request: {
+                    type: 'POST',
+                    url: 'http://localhost:3000/products',
+                    body: { name: 'String', price: 'Number'}
+                }
+            })
         })
         .catch(err => {
             console.log(err);
