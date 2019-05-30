dbd7d7bed0e8718eaa0bd44f3251fcf07f68f122
Rvail <naveedm@tcd.ie>
2019-05-30 17:01:08 +0100

    boilerplate orders replaced with RESTful API

--- api/models/order.js
+++ api/models/order.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 
 const orderSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    name: { type: String, required: true },
-    product: { type: mongoose.Schema.Types.ObjectId, ref : 'Product' },
+    product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
     quantity: { type: Number, default: 1 }
 });
 
